19667ce655aa1ac6cd2d3825ec472c97b5ea2ab7
nafuzi <gabrielnafuzi@gmail.com>
2020-06-13 12:38:10 -0300

    terminando

---
+++ scripts.js
@@ -0,0 +1,80 @@
+const textBox = document.querySelector('.textBox');
+const capilatizeFirst = document.querySelector('.capilatizeFirst');
+const capitalizeAll = document.querySelector('.capitalizeAll');
+const allToLowerCase = document.querySelector('.allToLowerCase');
+const alternative = document.querySelector('.alternative');
+const copyToClipBoard = document.querySelector('.copyToClipBoard');
+const downloadText = document.querySelector('.downloadText');
+
+capilatizeFirst.addEventListener('click', () => {
+  const textValue = textBox.value;
+
+  const textModified = capilatizeFirstLetter(textValue);
+
+  textBox.value = textModified;
+});
+
+capitalizeAll.addEventListener('click', () => {
+  const textValue = textBox.value;
+
+  textBox.value = textValue.toUpperCase();
+});
+
+allToLowerCase.addEventListener('click', () => {
+  const textValue = textBox.value;
+
+  textBox.value = textValue.toLowerCase();
+});
+
+alternative.addEventListener('click', () => {
+  const textValue = textBox.value;
+  let letters = textValue.split('');
+
+  for (let index = 0; index < letters.length; index++) {
+    if (!(index % 2 == 0)) {
+      letters[index] = letters[index].toUpperCase();
+    }
+  };
+
+  const lettersAfter = letters.join('');
+
+  textBox.value = lettersAfter;
+});
+
+copyToClipBoard.addEventListener('click', () => {
+  textBox.select();
+  document.execCommand('copy');
+
+});
+
+downloadText.addEventListener('click', () => {
+  const textValue = textBox.value;
+
+  downloadString(textValue);
+})
+
+function downloadString(text) {
+  const blob = new Blob([text], { type: 'text/plain' });
+  const a = document.createElement('a');
+
+  if (text === '') return;
+  
+  a.download = 'text.txt';
+  a.href = URL.createObjectURL(blob);
+  a.dataset.downloadurl = ['text/txt', a.download, a.href].join(':');
+  a.style.display = 'none';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+  setTimeout(() => {
+    URL.revokeObjectURL(a.href);
+  }, 1500);
+}
+
+function capilatizeFirstLetter(text) {
+  return text
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
+}
